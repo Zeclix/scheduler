5366c53b7808d2d965c07c42bea5ef9458b938b5
Zeclix <zeclix@yonsei.ac.kr>
2016-02-04 17:48:09 +0900

    something

--- scheduler.py
+++ scheduler.py
@@ -1,30 +1,33 @@
 import os
 import sys
 
-def enqueue():
-    #TODO
-    return
 
-def dequeue():
-    #TODO
-    return
+def enqueue(pList, PID, npTime, wTime=0, pTime=0):
+    pList.append([PID, npTime, wTime, pTime])
+    return 0
+
+
+def dequeue(pList):
+    return pList.pop()
+
 
 def lifo(CAT, pList):
-    #TODO
+    # TODO
     return
+
 
 def scheduler(sNum, CAT, pList):
     # 스케쥴러 선택
     # 1이면 FIFO, 2이면 SJF, 3이면 LIFO, 3만구현
-    if sNum==1:
+    if sNum == 1:
         print("Calling FIFO...")
         # fifo(CAT) # 구현안함
         return 0
-    elif sNum==2:
+    elif sNum == 2:
         print("Calling SJF...")
         # sjf(CAT) # 구현안함
         return 0
-    elif sNum==3:
+    elif sNum == 3:
         print("Calling LIFO...")
         lifo(CAT)
         return 0
@@ -33,14 +36,11 @@ def scheduler(sNum, CAT, pList):
         os._exit(1)
 
 
-
-
 # Input CPU Allocation Time
 while 1:
     CAT = int(input('Enter CPU Allocation Time : '))
-    if CAT>=3:
+    if CAT >= 3:
         break
-
 
 # file open
 try:
@@ -57,17 +57,20 @@ spList = f.readline()
 pList = spList.split()
 
 # 각 스트링을 int로 변환한다
-cnt=0
+cnt = 0
 for i in pList:
-    pList[cnt]=int(i)
-    cnt=cnt+1
+    pList[cnt] = int(i)
+    cnt = cnt + 1
 
 # pList의 각 구성 요소는 [PID, npTime, wTime, pTime]
 # npTime : 각 프로세스별 필요 소모시간
 # wTime : 각 프로세스 waiting time
 # pTime : 각 프로세스 professing time
+# pList[i] = [PID, npTime, wTime, pTime]
+
 for i in range(0, len(pList)):
-    pList[i] =[i, pList[i], 0, 0]
+    enqueue(pList, i, pList[i], 0, 0)
+
 
 # scheduler calling
 scheduler(int(input("Enter Scheduler # (1: FIFO / 2: SJF / 3: LIFO)")), CAT, pList)
